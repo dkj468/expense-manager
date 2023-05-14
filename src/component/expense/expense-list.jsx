58eb480ce8e9a605a96c1aa1ed943c399d7de2ed
Deepak Jain <deepak.pce1992@gmail.com>
2023-05-14 14:42:39 +0530

    showing data into data tabele. sorting,pagination

--- src/component/expense/expense-list.jsx
+++ src/component/expense/expense-list.jsx
@@ -1,16 +1,68 @@
 // container for expenses
 
-import Expense from "./expense";
+import DataTable from "react-data-table-component";
+// import Expense from "./expense";
 import classes from "./expense-list.module.css";
+
+const convertTimestamp = (timestamp) => {
+  let date = timestamp.toDate();
+  let mm = date.getMonth() + 1;
+  let dd = date.getDate();
+  let yyyy = date.getFullYear();
+
+  date = mm + "/" + dd + "/" + yyyy;
+  return date;
+};
+
+const customStyles = {
+  rows: {
+    style: {
+      minHeight: "36px", // override the row height
+    },
+  },
+  headCells: {
+    style: {
+      fontSize: "1.2rem",
+    },
+  },
+};
+const columns = [
+  {
+    name: "Expense",
+    selector: (row) => row.expenseName,
+  },
+  {
+    name: "Expense Account",
+    selector: (row) => row.expenseAccountName,
+    sortable: true,
+  },
+  {
+    name: "Date",
+    selector: (row) => convertTimestamp(row.expenseDate),
+    sortable: true,
+  },
+  {
+    name: "Amount",
+    selector: (row) => row.expenseAmount * 1,
+    sortable: true,
+  },
+];
 
 const ExpenseList = (props) => {
   const { expenses } = { ...props };
 
   return (
     <div className={classes["expense-container"]}>
-      {expenses.map((expense) => {
+      {/* {expenses.map((expense) => {
         return <Expense key={expense.id} {...expense} />;
-      })}
+      })} */}
+      <DataTable
+        columns={columns}
+        data={expenses}
+        pagination
+        responsive
+        customStyles={customStyles}
+      />
     </div>
   );
 };
