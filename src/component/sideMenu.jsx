7587748a1e3a3dd0bd4a1bb039acab383858e778
Deepak Kumar Jain <deepak.pce1992@gmail.com>
2023-06-08 11:41:52 +0000

    side menu design

--- src/component/sideMenu.jsx
+++ src/component/sideMenu.jsx
@@ -1,9 +1,25 @@
+import { NavLink } from "react-router-dom";
 import classes from "./sideMenu.module.css";
 
 const SideMenu = () => {
     return (
         <nav className={classes.sidemenu}>
-            <span> This is nav</span>
+            <div className={classes['menu-wrapper']}>
+                <NavLink to="/" className={classes.item}>Dashboard</NavLink>
+                <NavLink to="/" className={classes.item}>Expenses</NavLink>
+                <NavLink to="/" className={classes.item}>Expense Accounts</NavLink>
+                <NavLink to="/" className={classes.item}>Expense Category</NavLink>
+                <NavLink to="/" className={classes.item}>Profile</NavLink>
+                <NavLink to="/" className={classes.item + ' ' + classes['last-item']}>Logout</NavLink>
+
+                {/* <li><a>Dashboard</a></li>
+                <li><a>Expenses</a></li>
+                <li><a>Expense Account</a></li>
+                <li>Expense Category</li>
+                <li>Profile</li>
+                <li>Logout</li> */}
+
+            </div>
         </nav>
     )
 }
